7ef6f300200336d7337437fa0a6e18bf67f02ab9
Jiesh <jitesh.acharya2400@gmail.com>
2021-03-02 16:03:57 +0530

    Added header

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+//import React, { Component } from 'react';
+import React from 'react';
+
+/* This is functional component */
+const Header = function () {
+    return (
+        <div className="header">
+            Phone Directory
+        </div>
+    )
+}
+
+/* This is class component
+class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                Phone Directory
+            </div>
+        )
+    }
+}
+*/
+
+export default Header;
